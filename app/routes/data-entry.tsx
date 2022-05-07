9e7e62322140b389f88fd8180ff58329ce9f9a76
Stephen Cefali <scefali@sentry.io>
2022-05-06 18:09:03 -0700

    got kv working

--- app/routes/data-entry.tsx
+++ app/routes/data-entry.tsx
@@ -3,17 +3,17 @@ import type { KVNamespace } from "@cloudflare/workers-types";
 import { useLoaderData } from "@remix-run/react";
 import * as Sentry from "@sentry/react";
 
-// declare const DATAENTRY: KVNamespace;
+declare const DATAENTRY: KVNamespace;
 
 Sentry.init({
   dsn: "https://6f1e9f8dd238407185b0ecea0e2bbb51@o1218253.ingest.sentry.io/6360265",
 });
 
-export const action = async ({ request }) => {
+export const action = async ({ request }, env) => {
   try {
     const data = await request.json();
     console.log("data", data);
-    // await DATAENTRY.put(data.field, data.value);
+    await DATAENTRY.put(data.field, data.value);
     return json({ message: "ok" });
   } catch (err) {
     Sentry.captureException(err);
